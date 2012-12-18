c3b5a4d2caab70a61d21a790ebc414b956af0417
Aleksey <lek-o@yandex.ru>
2012-12-18 13:13:17 +0600

    Рисование крестика

--- VisualDraw_1/MainScreen.cs
+++ VisualDraw_1/MainScreen.cs
@@ -7,17 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+
 namespace VisualDraw_1
 {
     public partial class MainScreen : Form
     {
+        List<Cross> Shapes = new List<Cross>();
+
         public MainScreen()
         {
             InitializeComponent();
         }
         private void MainScreen_MouseMove(object sender, MouseEventArgs e)
         {
-            this.Text = Convert.ToString(e.X) + " . " + (e.Y);
+            this.Text = Convert.ToString(e.Location);
         }
+        private void MainScreen_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Cross f in Shapes)
+            {
+                f.DrawWith(e.Graphics);
+            }
+        }
+        private void MainScreen_MouseDown(object sender, MouseEventArgs e)
+        {
+            Shapes.Add(new Cross(e.X, e.Y));
+            this.Refresh();
+        }
+        public class Cross
+        {
+            int X, Y;
+            Pen p = new Pen(Color.Red);
+            public Cross(int _X, int _Y)
+            {
+                X = _X; Y = _Y;
+            }
+            public void DrawWith(Graphics g)
+            {
+                g.DrawLine(p, X - 4, Y - 4, X + 4, Y + 4);
+                g.DrawLine(p, X + 4, Y - 4, X - 4, Y + 4);
+            }
+        }
+
+        
     }
 }
