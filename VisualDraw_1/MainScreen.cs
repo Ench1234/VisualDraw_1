99fba90e91f8ec357d2e2d820ab4f5bc46cad492
Aleksey <lek-o@yandex.ru>
2012-12-25 12:50:20 +0600

    Вынести класс фигур в одельный файл

--- VisualDraw_1/MainScreen.cs
+++ VisualDraw_1/MainScreen.cs
@@ -47,40 +47,7 @@ namespace VisualDraw_1
         {
             IsShapeStart = !IsShapeStart;
         }
-        public abstract class Shape
-        {
-            public abstract void DrawWith(Graphics g);
-        }
-        public class Cross : Shape 
-        {
-            int X, Y;
-            Pen p = new Pen(Color.Red);
-            public Cross(int _X, int _Y)
-            {
-                X = _X; Y = _Y;
-            }
-            public override void DrawWith(Graphics g)
-            {
-                g.DrawLine(p, X - 4, Y - 4, X + 4, Y + 4);
-                g.DrawLine(p, X + 4, Y - 4, X - 4, Y + 4);
-            }
-        }
-
-        public class Line : Shape
-        {
-            Point C, F;            
-            Pen p = new Pen(Color.Blue);
-            public Line(Point _C, Point _F)
-            {
-                this.C = _C; this.F = _F;
-            }
-            public override void DrawWith(Graphics g)
-            {
-                g.DrawLine(p, C, F);
-            }
-        }
-
-        
+              
 
 
         
